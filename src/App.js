07813dc1921620ad29464b8bb769221583be3e98
OsamaAburabie <aburabie.osama@gmail.com>
2020-12-30 15:20:14 +0200

    project5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import DrinkList from "./components/DrinkList";
 import FoodList from "./components/FoodList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <FoodList />
-      <br />
-      <DrinkList />
-    </div>
+    <Router basename="/">
+      <div className="App">
+        {/* <Mynav /> */}
+        <Switch>
+          <Route path="/food" exact component={FoodList} />
+          <Route path="/drink" component={DrinkList} />
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 

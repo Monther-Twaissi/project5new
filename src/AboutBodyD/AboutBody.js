5857a2a85050f7443fa2b0df43ae17c6014e5775
OsamaAburabie <aburabie.osama@gmail.com>
2020-12-30 18:12:07 +0200

    project5

--- src/AboutBodyD/AboutBody.js
+++ src/AboutBodyD/AboutBody.js
@@ -1,48 +1,57 @@
-import React from 'react';
-import './AboutBody.css';
+import React from "react";
+import "./AboutBody.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function AboutBody() {
   return (
     <section>
-      <div className='about-container'>
-        <div className='about-row-container'>
-          <div className='about-column-66'>
+      <div className="about-container">
+        <div className="about-row-container">
+          <div className="about-column-66">
             <h2 className="about-header">GREAT TASTE, GOOD TIMES</h2>
             <p className="about-paragraph">
-            Here at Barney’s Burgers we take pride in the quality of the meat used for each and every burger. If it’s not perfect and grilled just the way you ordered it, we won’t send it out.  We guarantee the burger delivered to your table is the best burger you could have ordered.
+              Here at Barney’s Burgers we take pride in the quality of the meat
+              used for each and every burger. If it’s not perfect and grilled
+              just the way you ordered it, we won’t send it out. We guarantee
+              the burger delivered to your table is the best burger you could
+              have ordered.
             </p>
             <button className="btn btn-danger">OUR MENU</button>
           </div>
-          <div className='about-column-33'>
+          <div className="about-column-33">
             <img
-              alt='Burger Image'
-              className='about-image-style'
+              alt="Burger Image"
+              className="about-image-style"
               src="https://burgershop.templates.editmysite.com/uploads/8/8/9/6/88968090/burger-image-1-2x_orig.png"
             ></img>
           </div>
         </div>
       </div>
 
-      <div className='about-container'>
-        <div className='about-row-container'>
-          <div className='about-column-33'>
+      <div className="about-container">
+        <div className="about-row-container">
+          <div className="about-column-33">
             <img
-              alt='Burger Image'
-              className='about-image-style'
+              alt="Burger Image"
+              className="about-image-style"
               src="https://burgershop.templates.editmysite.com/uploads/8/8/9/6/88968090/burger-image-1-2x_orig.png"
             ></img>
           </div>
-          <div className='about-column-66'>
+          <div className="about-column-66">
             <h2 className="about-header">GREAT TASTE, GOOD TIMES</h2>
             <p className="about-paragraph">
-            Here at Barney’s Burgers we take pride in the quality of the meat used for each and every burger. If it’s not perfect and grilled just the way you ordered it, we won’t send it out.  We guarantee the burger delivered to your table is the best burger you could have ordered.
+              Here at Barney’s Burgers we take pride in the quality of the meat
+              used for each and every burger. If it’s not perfect and grilled
+              just the way you ordered it, we won’t send it out. We guarantee
+              the burger delivered to your table is the best burger you could
+              have ordered.
             </p>
             <button className="btn btn-danger">OUR MENU</button>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default AboutBody
+export default AboutBody;
--- src/LandingPageD/Landing.js
+++ src/LandingPageD/Landing.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import './Landing.css'
-
+import React from "react";
+import "./Landing.css";
 
 function Landing() {
-    return (
-        <section>
-            <div className="landing-section">
-            <div className="container">
+  return (
+    <section>
+      <div className="landing-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 col-sm-12">
+              <div className="inner-section">
+                <h1 className="header-landing">
+                  LOCAL BURGERS <br /> DONE RIGHT
+                </h1>
+                <p className="landing-paragraph">
+                  Join us for your next meal. We get the juices flowing
+                </p>
                 <div className="row">
-                    <div className="col-md-12 col-sm-12">
-                        <div className="inner-section">
-                            <h1 className="header-landing">LOCAL BURGERS <br/> DONE RIGHT</h1>
-                            <p className="landing-paragraph">Join us for your next meal. We get the juices flowing</p> 
-                            <div className="row">
-                                <div className="col-md-6 col-sm-6">
-                                <button className="btn btn-danger">OUR MENU</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  <div className="col-md-6 col-sm-6">
+                    <button className="btn btn-danger">OUR MENU</button>
+                  </div>
                 </div>
+              </div>
             </div>
-            </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Landing
+export default Landing;
